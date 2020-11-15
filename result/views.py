1ef721a781241f62a365d5947bfdcc5c8986f66c
bhagwanZaki <zaki.tarveen@gmail.com>
2020-11-15 23:56:14 +0530

    Final Commit'

--- result/views.py
+++ result/views.py
@@ -28,21 +28,17 @@ def createresult(request):
             year = form.cleaned_data['Year']
             year_name = year
             student = Student.objects.filter(current_year_id=year.id)
-            # attendance = Attendance.objects.filter(year_id=year.id)
-            # print(student)
             all_marks = []
             all_subject = []
             all_attendance = []
             submited_student = []
             for i,j in enumerate(student):
-                # print(i)
                 request_roll_year = Student.objects.filter(id=j.id).values()[0]['current_year_id']
                 request_roll = Student.objects.filter(id=j.id).values()[0]['Roll_number']
                 year = Year.objects.filter(id=request_roll_year)
                 try:
                     user = User.objects.filter(username=request_roll).values()[0]['id']
                     submited_student.append(request_roll)
-                    # print(submited_student)
                     for i in year:
                         subject = i.subjects.all()
                     assignment = Elements.objects.filter(subject__in=subject)
@@ -55,7 +51,6 @@ def createresult(request):
                         temp2 = []
                         temp3 = []
                         a = 0
-                        # subject_name.append(i.name)
                         all_subject.append(i.name)
                         for j in assignment:
                             if i == j.subject:
@@ -63,25 +58,21 @@ def createresult(request):
                                     a = Submissions.objects.filter(assignment_id=j.id,user_id=user).values()
                                     temp.append(Submissions.objects.filter(assignment_id=j.id,user_id=user).values()[0]['grade'])
                                     temp2.append(Elements.objects.filter(id= a[0]['assignment_id']).values()[0]['marks'])
-                                    # print(temp)
                                 except:
                                     temp.append(0)
                                     pass
                         marks.append(sum(temp))
-                        # print(marks)
                         assignment_marks.append(sum(temp2))
 
                         attendance = Attendance.objects.filter(subject_id=i.id)
 
                     all_marks.append(marks)
-                    # all_subject.append(subject_name)
-                    # all_attendance.append(temp3)
                 except :
 
                     ###################################################################
                     ######## if by chances the student username is not created ########
                     ###################################################################
-                    # marks = []
+                    
                     print(user,'failed')
               
 
@@ -98,7 +89,6 @@ def createresult(request):
                 total_att = []
                 try:
                     user = User.objects.filter(username=request_roll).values()[0]['id']
-                    # submited_student.append(request_roll)
                     for i in year:
                         subject = i.subjects.all()
                     att =[]
@@ -111,15 +101,10 @@ def createresult(request):
                             for stu in n.student.all():
                                 check = stu.Roll_number
                                 if check == request_roll:
-                                    # print(check , 'present')
                                     a += 1
-                                    # print(a)
                         att.append(a)
                         total_att.append(b)
-                    # print(att)
                     all_att.append(att)
-                    # print(all_att)
-                    # print(total_att)
                 except Exception as e:
                     print(e)
                     print('code crahed')
@@ -137,17 +122,25 @@ def createresult(request):
             print('total marks of each subject ',assignment_marks)
             print('student attendance ',all_att)
             print('total attendance of each subject ',total_att)
-            # test(all_subject,submited_student,all_marks,assignment_marks,all_att,total_att,year_name)
+            
             
             ###################################################
             ############### Writing in csv files ##############
             ###################################################
+
+
+
+            #######################################################
+            ############ Attendance Marksheet area ################
+            #######################################################
 
             response = HttpResponse(content_type='text/csv')
             filename = '{}.csv'.format(year_name)
             print(filename)  
             response['Content-Disposition'] = 'attachment; filename={}'.format(filename)  
             writer = csv.writer(response)  
+
+            writer.writerow(['','','','Marksheet Record Report'])
 
             header = ['Roll Number']
             for i in all_subject:
@@ -171,10 +164,37 @@ def createresult(request):
 
                 print(row_data)
                 writer.writerow(row_data)  
-
-            return response  
             
-            pass
+            writer.writerow([''])
+            writer.writerow([''])
+            writer.writerow([''])
+            writer.writerow([''])
+            writer.writerow([''])
+            writer.writerow([''])
+
+            #######################################################
+            ############ Attendance Marksheet area ################
+            #######################################################
+
+            writer.writerow(['','','','Attendance Record Report'])
+            header = ['Roll Number']
+            for i in all_subject:
+                header.append(i)
+            header.append('Total Attendance in percentage')
+            header.append('Total Attendance in Number')
+            header.append('Overall Attendance in Number')
+            writer.writerow(header)  
+            for k in range(len(submited_student)):
+                row_data = []
+                row_data.append(submited_student[k])
+                for j in range(len(total_att)):
+                    row_data.append((all_att[k][j]/total_att[k])*100)
+                row_data.append((sum(all_att[k])/sum(total_att))*100)
+                row_data.append(sum(all_att[k]))
+                row_data.append(sum(total_att))
+
+                writer.writerow(row_data)
+            return response  
 
 def test(all_subject,submited_student,all_marks,assignment_marks,all_att,total_att,year_name):
     header = ['Roll Number']
